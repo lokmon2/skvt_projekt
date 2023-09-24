f4bfa8ca6bc7c10fb891f3bde9580e039ff21e6a
lokmon2 <lokmon2@gmail.com>
2023-09-24 15:34:03 +0200

    Code CleanUp

--- Source/skvt_projekt/CustomCamera.cpp
+++ Source/skvt_projekt/CustomCamera.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CustomCamera.h"
 
 float UCustomCamera::CustomLerp(float current, float target, float alpha, float crouchingZoom, bool isCrouching) {
--- Source/skvt_projekt/CustomCamera.h
+++ Source/skvt_projekt/CustomCamera.h
@@ -1,19 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "CustomCamera.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SKVT_PROJEKT_API UCustomCamera : public UBlueprintFunctionLibrary {
 	GENERATED_BODY()
 
-	UFUNCTION(BlueprintCallable, Category="Custom Camera")
+	UFUNCTION(BlueprintPure, Category="Custom Camera Functions")
 	static float CustomLerp(float current, float target, float alpha, float crouchingZoom, bool isCrouching);
-	
 };
